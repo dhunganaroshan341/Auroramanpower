7ef9fb90ed48af9463a8d42f24a9f6b7f45b6b5a
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-19 00:45:29 +0545

    Auto commit changes

--- resources/views/Admin/layout/header.blade.php
+++ resources/views/Admin/layout/header.blade.php
@@ -7,6 +7,9 @@
 
     <!-- jQuery -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
+    <!-- Add SortableJS CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/Sortable.min.js"></script>
+
 
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
     <link rel="stylesheet" href="{{ asset('admin/vendors/mdi/css/materialdesignicons.min.css') }}">
