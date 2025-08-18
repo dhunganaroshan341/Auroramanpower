5d8aa4658b1a4f001e3dc4c97e047f0b1093c754
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-19 00:48:50 +0545

    Auto commit changes

--- public/js/admin/section-content/reorder.js
+++ public/js/admin/section-content/reorder.js
@@ -1,30 +1,3 @@
-
-$(document).ready(function () {
-$('#reorderBtn').on('click', function () {
-    let categoryId = $('#categoryFilter').val();
-    if (!categoryId) return;
-
-    $.ajax({
-        url: `/admin/section-content/${categoryId}/list`,
-        type: "GET",
-        success: function (items) {
-            let listHtml = '';
-            items.forEach(item => {
-                listHtml += `<li class="list-group-item" data-id="${item.id}">
-                                <i class="fas fa-arrows-alt me-2"></i> ${item.title}
-                             </li>`;
-            });
-            $('#sortableList').html(listHtml);
-
-            // Initialize SortableJS
-            new Sortable(document.getElementById('sortableList'), {
-                animation: 150
-            });
-
-            $('#reorderModal').modal('show');
-        }
-    });
-});
 $('#saveOrderBtn').on('click', function () {
     let order = [];
     $('#sortableList li').each(function (index) {
@@ -43,8 +16,23 @@ $('#saveOrderBtn').on('click', function () {
         },
         success: function () {
             $('#reorderModal').modal('hide');
-            sectionCategoryTable.ajax.reload();
+            sectionContentTable.ajax.reload(); // reload table
+
+            // SweetAlert success notification
+            Swal.fire({
+                icon: 'success',
+                title: 'Reordered!',
+                text: 'Section content order has been updated.',
+                timer: 2000,
+                showConfirmButton: false
+            });
+        },
+        error: function (err) {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops!',
+                text: 'Failed to update order.',
+            });
         }
     });
-});
 });
