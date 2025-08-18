d9c609f909a2458b1fb3182d5ddcde6842375fbe
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-18 17:20:51 +0545

    Auto commit changes

--- resources/views/Admin/layout/navbar.blade.php
+++ resources/views/Admin/layout/navbar.blade.php
@@ -2,7 +2,6 @@
 
     {{-- === CORE SETTINGS === --}}
     <li class="nav-item nav-category">Core</li>
-
     <li class="nav-item">
         <a class="nav-link" href="{{ route('admin.dashboard') }}">
             <i class="mdi mdi-view-dashboard menu-icon"></i>
@@ -11,21 +10,19 @@
     </li>
     <li class="nav-item">
         <a class="nav-link" href="{{ route('admin.user') }}">
-            <i class="mdi mdi-account menu-icon"></i>
+            <i class="mdi mdi-account-multiple menu-icon"></i>
             <span class="menu-title">Users</span>
         </a>
     </li>
     <li class="nav-item">
         <a class="nav-link" href="{{ route('admin.setting.index') }}">
-            <i class="mdi mdi-cog menu-icon"></i>
-            <span class="menu-title">General Settings</span>
+            <i class="mdi mdi-cog-outline menu-icon"></i>
+            <span class="menu-title">Settings</span>
         </a>
     </li>
 
-
-    {{-- === TRAVEL OPERATIONS === --}}
+    {{-- === OPERATIONS === --}}
     <li class="nav-item nav-category">Operations</li>
-
     <li class="nav-item">
         <a class="nav-link" data-bs-toggle="collapse" href="#packageMenu" role="button" aria-expanded="false"
             aria-controls="packageMenu">
@@ -35,49 +32,41 @@
         </a>
         <div class="collapse" id="packageMenu">
             <ul class="nav flex-column sub-menu">
-                <li class="nav-item"><a class="nav-link" href="{{ route('admin.tour-packages.index') }}">Manage</a>
-                </li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('admin.bookings.index') }}">Bookings</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('admin.tour-packages.index') }}"><i
+                            class="mdi mdi-briefcase-outline"></i> Manage Packages</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('admin.bookings.index') }}"><i
+                            class="mdi mdi-calendar-check"></i> Bookings</a></li>
             </ul>
         </div>
     </li>
 
-
-
     {{-- === CONTENT MANAGEMENT === --}}
-    <li class="nav-item nav-category">Content</li>
-
-
-
-
-    <li class="nav-item nav-category">Page & sections Management</li>
-
-    <li class="nav-item">
+    <li class="nav-item nav-category">Content Management</li>
     <li class="nav-item">
         <a class="nav-link" href="{{ route('admin.pages.index') }}">
-            <i class="mdi mdi-file-document menu-icon"></i>
-            <span class="menu-title">General Page & Section</span>
+            <i class="mdi mdi-file-document-outline menu-icon"></i>
+            <span class="menu-title">Pages & Sections</span>
         </a>
     </li>
-    <a class="nav-link" data-bs-toggle="collapse" href="#packageMenu" role="button" aria-expanded="false"
-        aria-controls="packageMenu">
-        <i class="mdi mdi-map-marker-path menu-icon"></i>
-        <span class="menu-title">Manage sections </span>
-        <i class="menu-arrow"></i>
-    </a>
-    <div class="collapse" id="packageMenu">
-        <ul class="nav flex-column sub-menu">
-            <li class="nav-item"><a class="nav-link" href="{{ route('admin.section-category.index') }}">Category</a>
-            </li>
-            <li class="nav-item"><a class="nav-link" href="{{ route('admin.section-content.index') }}">Section
-                    Content</a>
-            </li>
-        </ul>
-    </div>
+    <li class="nav-item">
+        <a class="nav-link" data-bs-toggle="collapse" href="#sectionMenu" role="button" aria-expanded="false"
+            aria-controls="sectionMenu">
+            <i class="mdi mdi-view-list menu-icon"></i>
+            <span class="menu-title">Manage Sections</span>
+            <i class="menu-arrow"></i>
+        </a>
+        <div class="collapse" id="sectionMenu">
+            <ul class="nav flex-column sub-menu">
+                <li class="nav-item"><a class="nav-link" href="{{ route('admin.section-category.index') }}"><i
+                            class="mdi mdi-shape-outline"></i> Section Categories</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('admin.section-content.index') }}"><i
+                            class="mdi mdi-format-list-bulleted"></i> Section Content</a></li>
+            </ul>
+        </div>
     </li>
 
-
-
+    {{-- === BANNERS === --}}
+    <li class="nav-item nav-category">Banners</li>
     <li class="nav-item">
         <a class="nav-link" data-bs-toggle="collapse" href="#bannerMenu" role="button" aria-expanded="false"
             aria-controls="bannerMenu">
@@ -87,37 +76,36 @@
         </a>
         <div class="collapse" id="bannerMenu">
             <ul class="nav flex-column sub-menu">
-                {{-- <li class="nav-item"><a class="nav-link" href="{{ route('admin.homeslide') }}">Home Slider</a></li> --}}
-                <li class="nav-item"><a class="nav-link" href="{{ route('admin.banner.video.index') }}">Video Banner</a>
-                </li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('admin.page-banner.index') }}">Page Banner</a>
-                </li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('admin.call-to-action.index') }}">CTA
-                        Banner</a>
-                </li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('admin.banner.video.index') }}"><i
+                            class="mdi mdi-video"></i> Video Banner</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('admin.page-banner.index') }}"><i
+                            class="mdi mdi-image"></i> Page Banner</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('admin.call-to-action.index') }}"><i
+                            class="mdi mdi-bullhorn-outline"></i> CTA Banner</a></li>
             </ul>
         </div>
     </li>
 
+    {{-- === SERVICES & TESTIMONIALS === --}}
+    <li class="nav-item nav-category">Services & Social Proof</li>
     <li class="nav-item">
         <a class="nav-link" href="{{ route('admin.service.index') }}">
             <i class="mdi mdi-face-agent menu-icon"></i>
-            <span class="menu-title">service page</span>
+            <span class="menu-title">Services</span>
         </a>
     </li>
     <li class="nav-item">
         <a class="nav-link" href="{{ route('admin.testimonial') }}">
-            <i class="mdi mdi-animation menu-icon"></i>
+            <i class="mdi mdi-comment-account menu-icon"></i>
             <span class="menu-title">Testimonials</span>
         </a>
     </li>
     <li class="nav-item">
         <a class="nav-link" href="{{ route('admin.achievements.index') }}">
-            <i class="mdi mdi-trophy menu-icon"></i>
-            <span class="menu-title">Counter / Achievements</span>
+            <i class="mdi mdi-trophy-outline menu-icon"></i>
+            <span class="menu-title">Achievements</span>
         </a>
     </li>
-
     <li class="nav-item">
         <a class="nav-link" href="{{ route('admin.client.index') }}">
             <i class="mdi mdi-account-group-outline menu-icon"></i>
@@ -131,53 +119,33 @@
         </a>
     </li>
 
-
-
-    {{-- === MARKETING & MEDIA === --}}
+    {{-- === MEDIA & MARKETING === --}}
     <li class="nav-item nav-category">Media & Marketing</li>
-
     <li class="nav-item">
         <a class="nav-link" href="{{ route('admin.notice.index') }}">
             <i class="mdi mdi-bullhorn menu-icon"></i>
-            <span class="menu-title">Notice</span>
+            <span class="menu-title">Notices</span>
         </a>
     </li>
-
     <li class="nav-item">
-        <a class="nav-link" data-bs-toggle="collapse" href="#blogSubmenu" role="button" aria-expanded="false"
-            aria-controls="blogSubmenu">
+        <a class="nav-link" data-bs-toggle="collapse" href="#blogMenu" role="button" aria-expanded="false"
+            aria-controls="blogMenu">
             <i class="mdi mdi-post-outline menu-icon"></i>
             <span class="menu-title">Blogs</span>
             <i class="menu-arrow"></i>
         </a>
-        <div class="collapse" id="blogSubmenu">
+        <div class="collapse" id="blogMenu">
             <ul class="nav flex-column sub-menu">
-                <li class="nav-item"><a class="nav-link" href="{{ route('admin.category') }}">Category</a></li>
-                <li class="nav-item"><a class="nav-link" href="{{ route('admin.post') }}">Post</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('admin.category') }}"><i
+                            class="mdi mdi-tag-outline"></i> Blog Categories</a></li>
+                <li class="nav-item"><a class="nav-link" href="{{ route('admin.post') }}"><i
+                            class="mdi mdi-note-text-outline"></i> Blog Posts</a></li>
             </ul>
         </div>
     </li>
 
     {{-- === UTILITIES === --}}
     <li class="nav-item nav-category">Utilities</li>
-
     <li class="nav-item">
         <a class="nav-link" href="{{ route('admin.contact.index') }}">
-            <i class="mdi mdi-contacts menu-icon"></i>
-            <span class="menu-title">Contact</span>
-        </a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('admin.newsletters.index') }}">
-            <i class="mdi mdi-contacts menu-icon"></i>
-            <span class="menu-title">NewsLetters</span>
-        </a>
-    </li>
-
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('admin.logout') }}">
-            <i class="mdi mdi-logout menu-icon text-danger"></i>
-            <span class="menu-title text-danger">Logout</span>
-        </a>
-    </li>
-</ul>
+            <i class="mdi
