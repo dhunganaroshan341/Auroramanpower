f9afe90022a4d783ddd5fe6a194407abda09b3af
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-20 12:50:40 +0545

    Auto commit changes

--- resources/views/components/testimonial-section.blade.php
+++ resources/views/components/testimonial-section.blade.php
@@ -1,34 +1,32 @@
- <section class="testimonial pb-6">
-     <div class="container">
-         <div class="section-title text-center mb-5 pb-2 w-50 mx-auto">
-             <h2 class="m-0">What <span>People Say About Us</span></h2>
-             <p class="mb-0">
-                 Travel has helped us to understand the meaning of life and it has helped us become better
-                 people. Each time we travel, we see the world with new eyes.
-             </p>
-         </div>
+<!-- testimonial-style-two -->
+<section class="testimonial-style-two pt_70 pb_120">
+    <div class="pattern-layer" style="background-image: url('{{ asset('assets/images/shape/shape-17.png') }}')"></div>
+    <div class="auto-container">
+        <div class="sec-title centred pb_60 sec-title-animation animation-style2">
+            <span class="sub-title mb_10 title-animation">Testimonials</span>
+            <h2 class="title-animation">Love From Users</h2>
+        </div>
 
-         <div class="review-slider owl-carousel">
-             @foreach ($testimonials as $testimonial)
-                 <div class="item">
-                     <div class="testimonial-item1 text-center">
-                         <div class="details">
-                             <p class="m-0">{{ $testimonial->description }}</p>
-                         </div>
-                         <div class="author-info mt-2">
-                             <a href="#">
-                                 <img alt="client"
-                                     src="{{ $testimonial->image_url ?? asset('template/yatri_world/main-file/images/User.png') }}" />
-                             </a>
-                             <div class="author-title">
-                                 <h4 class="m-0 pink">{{ $testimonial->name }}</h4>
-                                 <span>{{ $testimonial->designation }}</span>
-                             </div>
-                         </div>
-                         <i class="fa fa-quote-left mb-2"></i>
-                     </div>
-                 </div>
-             @endforeach
-         </div>
-     </div>
- </section>
+        <div class="two-item-carousel owl-carousel owl-theme owl-nav-none">
+            @foreach ($testimonials as $testimonial)
+                <div class="testimonial-block-two">
+                    <div class="inner-box">
+                        <div class="icon-box">
+                            <img src="{{ asset('assets/images/icons/icon-11.png') }}" alt="">
+                        </div>
+                        <div class="author-box">
+                            <figure class="thumb-box">
+                                <img src="{{ $testimonial->image_url ?? asset('template/yatri_world/main-file/images/User.png') }}"
+                                    alt="{{ $testimonial->name }}">
+                            </figure>
+                            <h4>{{ $testimonial->name }}</h4>
+                            <span class="designation">{{ $testimonial->designation }}</span>
+                        </div>
+                        <p>{{ $testimonial->description }}</p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</section>
+<!-- testimonial-style-two end -->
