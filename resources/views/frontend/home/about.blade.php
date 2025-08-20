51f4aa72ff419798fd1fd77ec8fe8d9d7e21aa9a
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-20 12:59:13 +0545

    Auto commit changes

--- resources/views/frontend/home/about.blade.php
+++ resources/views/frontend/home/about.blade.php
@@ -43,21 +43,24 @@
                         <div class="image-inner">
                             <div class="image-box mr_15">
                                 <figure class="image image-1 mb_15"><img
-                                        src="{{ asset('assets/images/resource/aurora-1.jpg') }}" alt="Aurora Team"></figure>
-                                <figure class="image image-2"><img src="{{ asset('assets/images/resource/aurora-2.jpg') }}"
-                                        alt="Aurora Work"></figure>
+                                        src="{{ asset('assets/images/resource/about-aurora-1.jpg') }}" alt="">
+                                </figure>
+                                <figure class="image image-2"><img
+                                        src="{{ asset('assets/images/resource/about-aurora-2.jpg') }}" alt="">
+                                </figure>
                             </div>
                             <div class="image-box">
                                 <figure class="image image-3 mb_15"><img
-                                        src="{{ asset('assets/images/resource/aurora-3.jpg') }}" alt="Aurora Office">
+                                        src="{{ asset('assets/images/resource/about-aurora-3.jpg') }}" alt="">
                                 </figure>
-                                <figure class="image image-4"><img src="{{ asset('assets/images/resource/aurora-4.jpg') }}"
-                                        alt="Aurora Innovation"></figure>
+                                <figure class="image image-4"><img
+                                        src="{{ asset('assets/images/resource/about-aurora-4.jpg') }}" alt="">
+                                </figure>
                             </div>
                             <div class="support-box">
                                 <div class="icon-box"><i class="icon-28"></i></div>
-                                <span>Support Anytime</span>
-                                <h4><a href="tel:+123456789">+123 (456) 789</a></h4>
+                                <span>Online Support</span>
+                                <h4><a href="tel:+977123456789">+977 123 456 789</a></h4>
                             </div>
                         </div>
                     </div>
@@ -66,31 +69,30 @@
                     <div class="content_block_five">
                         <div class="content-box">
                             <div class="sec-title pb_40 sec-title-animation animation-style2">
-                                <span class="sub-title mb_10 title-animation">About Aurora</span>
-                                <h2 class="title-animation">Innovation Meets <span>Creativity</span></h2>
-                                <p class="title-animation">Aurora transforms ideas into digital experiences that inspire and
-                                    engage audiences worldwide.</p>
+                                <span class="sub-title mb_10 title-animation">About us</span>
+                                <h2 class="title-animation">Aurora <span>Nepal</span> HR</h2>
+                                <p class="title-animation">Providing top HR solutions and recruitment services in Nepal.</p>
                             </div>
                             <div class="inner-box clearfix">
                                 <div class="single-item">
                                     <div class="icon-box"><i class="icon-29"></i></div>
-                                    <h4><a href="{{ route('jobDetails') }}">Cutting-edge Solutions</a></h4>
+                                    <h4><a href="{{ route('jobDetails') }}">Talent Acquisition</a></h4>
                                     <span>2025</span>
                                 </div>
                                 <div class="single-item">
                                     <div class="icon-box"><i class="icon-30"></i></div>
-                                    <h4><a href="{{ route('jobDetails') }}">Loved by Clients</a></h4>
-                                    <span>Global</span>
+                                    <h4><a href="{{ route('jobDetails') }}">Employee Management</a></h4>
+                                    <span>2025</span>
                                 </div>
                                 <div class="single-item">
                                     <div class="icon-box"><i class="icon-31"></i></div>
-                                    <h4><a href="{{ route('jobDetails') }}">Pioneering Team</a></h4>
+                                    <h4><a href="{{ route('jobDetails') }}">Leadership Consulting</a></h4>
                                     <span>2025</span>
                                 </div>
                                 <div class="single-item">
                                     <div class="icon-box"><i class="icon-32"></i></div>
-                                    <h4><a href="{{ route('jobDetails') }}">Reliable Support</a></h4>
-                                    <span>24/7</span>
+                                    <h4><a href="{{ route('jobDetails') }}">Support Services</a></h4>
+                                    <span>2025</span>
                                 </div>
                             </div>
                         </div>
@@ -106,16 +108,21 @@
         <div class="bg-color"></div>
         <div class="auto-container">
             <div class="inner-container">
-                @foreach (range(1, 5) as $i)
-                    <div class="clients-box">
-                        <figure class="clients-logo"><a href="{{ route('index') }}"><img
-                                    src="{{ asset('assets/images/clients/aurora-client-' . $i . '.png') }}" alt=""></a>
-                        </figure>
-                        <figure class="overlay-logo"><a href="{{ route('index') }}"><img
-                                    src="{{ asset('assets/images/clients/aurora-client-' . $i . '.png') }}" alt=""></a>
-                        </figure>
-                    </div>
-                @endforeach
+                <div class="clients-box">
+                    <figure class="clients-logo"><a href="{{ route('index') }}"><img
+                                src="{{ asset('assets/images/clients/clients-aurora-1.png') }}" alt=""></a></figure>
+                    <figure class="overlay-logo"><a href="{{ route('index') }}"><img
+                                src="{{ asset('assets/images/clients/clients-aurora-1.png') }}" alt=""></a>
+                    </figure>
+                </div>
+                <div class="clients-box">
+                    <figure class="clients-logo"><a href="{{ route('index') }}"><img
+                                src="{{ asset('assets/images/clients/clients-aurora-2.png') }}" alt=""></a>
+                    </figure>
+                    <figure class="overlay-logo"><a href="{{ route('index') }}"><img
+                                src="{{ asset('assets/images/clients/clients-aurora-2.png') }}" alt=""></a>
+                    </figure>
+                </div>
             </div>
         </div>
     </section>
@@ -123,12 +130,11 @@
 
     <!-- chooseus-section -->
     <section class="chooseus-section alternat-3 pt_120 pb_90">
-        <div class="pattern-layer" style="background-image: url('{{ asset('assets/images/shape/aurora-shape.png') }}')">
-        </div>
+        <div class="pattern-layer" style="background-image: url('{{ asset('assets/images/shape/shape-23.png') }}')"></div>
         <div class="auto-container">
             <div class="sec-title light centred pb_60 sec-title-animation animation-style2">
-                <span class="sub-title mb_10 title-animation">Why Aurora</span>
-                <h2 class="title-animation">Why Choose Aurora</h2>
+                <span class="sub-title mb_10 title-animation">Why Us</span>
+                <h2 class="title-animation">Why Choose Aurora Nepal</h2>
             </div>
             <div class="inner-container">
                 <div class="row clearfix">
@@ -136,8 +142,8 @@
                         <div class="chooseus-block-one">
                             <div class="inner-box">
                                 <div class="icon-box"><i class="icon-4"></i></div>
-                                <h3><a href="{{ route('index') }}">Innovation</a></h3>
-                                <p>Transforming ideas into meaningful digital experiences that leave a mark.</p>
+                                <h3><a href="{{ route('index') }}">Top Talent</a></h3>
+                                <p>Helping companies in Nepal find and retain the best talent.</p>
                                 <div class="link"><a href="{{ route('index') }}">Learn More<i class="icon-7"></i></a>
                                 </div>
                             </div>
@@ -147,8 +153,8 @@
                         <div class="chooseus-block-one">
                             <div class="inner-box">
                                 <div class="icon-box"><i class="icon-5"></i></div>
-                                <h3><a href="{{ route('index') }}">Global Impact</a></h3>
-                                <p>Solutions that resonate with clients worldwide, driving growth and engagement.</p>
+                                <h3><a href="{{ route('index') }}">Compliance</a></h3>
+                                <p>Ensuring HR processes comply with Nepalese regulations.</p>
                                 <div class="link"><a href="{{ route('index') }}">Learn More<i class="icon-7"></i></a>
                                 </div>
                             </div>
@@ -158,8 +164,8 @@
                         <div class="chooseus-block-one">
                             <div class="inner-box">
                                 <div class="icon-box"><i class="icon-6"></i></div>
-                                <h3><a href="{{ route('index') }}">Trusted Team</a></h3>
-                                <p>Our experienced team ensures projects are delivered on-time and beyond expectations.</p>
+                                <h3><a href="{{ route('index') }}">Employee Growth</a></h3>
+                                <p>Providing training and development programs for employees.</p>
                                 <div class="link"><a href="{{ route('index') }}">Learn More<i class="icon-7"></i></a>
                                 </div>
                             </div>
@@ -170,69 +176,4 @@
         </div>
     </section>
     <!-- chooseus-section end -->
-
-    <!-- team-section -->
-    <section class="team-section z_1 centred pt_0 pb_0">
-        <div class="auto-container">
-            <div class="sec-title pb_60 sec-title-animation animation-style2">
-                <span class="sub-title mb_10 title-animation">Aurora Team</span>
-                <h2 class="title-animation">Meet Our Visionaries</h2>
-            </div>
-            <div class="row clearfix">
-                @foreach (range(1, 4) as $i)
-                    <div class="col-lg-3 col-md-6 col-sm-12 team-block">
-                        <div class="team-block-one wow fadeInUp animated" data-wow-delay="{{ ($i - 1) * 200 }}ms"
-                            data-wow-duration="1500ms">
-                            <div class="inner-box">
-                                <div class="image-box">
-                                    <figure class="image"><img
-                                            src="{{ asset('assets/images/team/aurora-team-' . $i . '.jpg') }}"
-                                            alt=""></figure>
-                                    <figure class="overlay-image"><img
-                                            src="{{ asset('assets/images/team/aurora-team-' . $i . '.jpg') }}"
-                                            alt=""></figure>
-                                </div>
-                                <div class="lower-content">
-                                    <h3><a href="{{ route('index') }}">Member {{ $i }}</a></h3>
-                                    <span class="designation">Position {{ $i }}</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-        </div>
-    </section>
-    <!-- team-section end -->
-
-    <!-- testimonial-style-two -->
-    <section class="testimonial-style-two pt_70 pb_120">
-        <div class="pattern-layer"
-            style="background-image: url('{{ asset('assets/images/shape/aurora-testimonial-shape.png') }}')"></div>
-        <div class="auto-container">
-            <div class="sec-title centred pb_60 sec-title-animation animation-style2">
-                <span class="sub-title mb_10 title-animation">Testimonials</span>
-                <h2 class="title-animation">What Clients Say</h2>
-            </div>
-            <div class="two-item-carousel owl-carousel owl-theme owl-nav-none">
-                @foreach (range(1, 4) as $i)
-                    <div class="testimonial-block-two">
-                        <div class="inner-box">
-                            <div class="icon-box"><img src="{{ asset('assets/images/icons/aurora-quote.png') }}"
-                                    alt=""></div>
-                            <div class="author-box">
-                                <figure class="thumb-box"><img
-                                        src="{{ asset('assets/images/resource/aurora-testimonial-' . $i . '.png') }}"
-                                        alt=""></figure>
-                                <h4>Client {{ $i }}</h4>
-                                <span class="designation">CEO, Company {{ $i }}</span>
-                            </div>
-                            <p>Aurora delivered beyond expectations, blending creativity and technology seamlessly.</p>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-        </div>
-    </section>
-    <!-- testimonial-style-two end -->
 @endsection
