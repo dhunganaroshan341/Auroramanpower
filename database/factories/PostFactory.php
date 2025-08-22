40fde5c20dac21f6855706e57c1ea8287af6871a
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-08-22 13:48:51 +0545

    Auto commit changes

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -6,15 +6,13 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use App\Models\Post;
 use App\Models\Tag;
+use App\Models\Category;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
  */
 class PostFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     */
     public function definition(): array
     {
         $title = $this->faker->sentence;
@@ -22,19 +20,20 @@ class PostFactory extends Factory
         return [
             'title' => $title,
             'slug' => Str::slug($title),
-            'category_id' => $this->faker->numberBetween(1, 5),
             'description' => $this->faker->paragraphs(3, true),
             'created_by' => 1,
         ];
     }
 
-    /**
-     * Attach random tags after creating the post.
-     */
     public function configure()
     {
         return $this->afterCreating(function (Post $post) {
-            $tagIds = Tag::inRandomOrder()->take(rand(1, 3))->pluck('id');
+            // Attach random categories (1-3)
+            $categoryIds = Category::inRandomOrder()->take(rand(1,3))->pluck('id');
+            $post->categories()->sync($categoryIds);
+
+            // Attach random tags (1-3)
+            $tagIds = Tag::inRandomOrder()->take(rand(1,3))->pluck('id');
             $post->tags()->sync($tagIds);
         });
     }
