92f59d2d937d346ee52e95f4457aa2f3dcf2fdf0
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-09-17 05:41:53 +0545

    Auto commit changes

--- resources/views/frontend/dynamic-page.blade.php
+++ resources/views/frontend/dynamic-page.blade.php
@@ -2,37 +2,42 @@
 
 @php
 
-    $css =
-        '<link href="' .
+$css =
+'
+<link href="' .
         asset('assets/css/module-css/page-title.css') .
         '" rel="stylesheet">
-            <link href="' .
+<link href="' .
         asset('assets/css/module-css/about.css') .
         '" rel="stylesheet">
-            <link href="' .
+<link href="' .
         asset('assets/css/module-css/clients.css') .
         '" rel="stylesheet">
-            <link href="' .
+<link href="' .
         asset('assets/css/module-css/chooseus.css') .
         '" rel="stylesheet">
-            <link href="' .
+<link href="' .
         asset('assets/css/module-css/industries.css') .
         '" rel="stylesheet">
-            <link href="' .
+<link href="' .
         asset('assets/css/module-css/team.css') .
         '" rel="stylesheet">
-            <link href="' .
+<link href="' .
         asset('assets/css/module-css/testimonial.css') .
         '" rel="stylesheet">
-            <link href="' .
+<link href="' .
         asset('assets/css/module-css/subscribe.css') .
         '" rel="stylesheet">
-            <link href="' .
+<link href="' .
         asset('assets/css/module-css/footer.css') .
         '" rel="stylesheet">';
-    $title = $content->title ?? 'Aurora Nepal';
-    $subTitle = $content->content_sub_heading ?? 'Nepal\'s leading manpower company';
-@endphp
+$title = $content->title ?? 'Aurora Nepal';
+if(isset($content->title&& $content->title != '' )){
+$subTitle = $content->content_sub_heading ?? '';}
+else {
+# code...
+$subTitle = 'Nepal\'s leading manpower company';
+}
 
 @section('content')
     <Section>
