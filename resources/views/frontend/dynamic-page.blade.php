014106982198580e7baef36fe12662d277a0f4db
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-09-17 05:43:41 +0545

    Auto commit changes

--- resources/views/frontend/dynamic-page.blade.php
+++ resources/views/frontend/dynamic-page.blade.php
@@ -1,43 +1,46 @@
 @extends('frontend.layouts.layout')
 
 @php
-
-$css =
-'
-<link href="' .
+    $css =
+        '
+    <link href="' .
         asset('assets/css/module-css/page-title.css') .
         '" rel="stylesheet">
-<link href="' .
+    <link href="' .
         asset('assets/css/module-css/about.css') .
         '" rel="stylesheet">
-<link href="' .
+    <link href="' .
         asset('assets/css/module-css/clients.css') .
         '" rel="stylesheet">
-<link href="' .
+    <link href="' .
         asset('assets/css/module-css/chooseus.css') .
         '" rel="stylesheet">
-<link href="' .
+    <link href="' .
         asset('assets/css/module-css/industries.css') .
         '" rel="stylesheet">
-<link href="' .
+    <link href="' .
         asset('assets/css/module-css/team.css') .
         '" rel="stylesheet">
-<link href="' .
+    <link href="' .
         asset('assets/css/module-css/testimonial.css') .
         '" rel="stylesheet">
-<link href="' .
+    <link href="' .
         asset('assets/css/module-css/subscribe.css') .
         '" rel="stylesheet">
-<link href="' .
+    <link href="' .
         asset('assets/css/module-css/footer.css') .
-        '" rel="stylesheet">';
-$title = $content->title ?? 'Aurora Nepal';
-if(isset($content->title&& $content->title != '' )){
-$subTitle = $content->content_sub_heading ?? '';}
-else {
-# code...
-$subTitle = 'Nepal\'s leading manpower company';
-}
+        '" rel="stylesheet">
+    ';
+
+    $title = $content->title ?? 'Aurora Nepal';
+
+    // If dynamic title exists, check for subtitle
+    if (!empty($content->title)) {
+        $subTitle = $content->content_sub_heading ?? '';
+    } else {
+        $subTitle = "Nepal's leading manpower company";
+    }
+@endphp
 
 @section('content')
     <Section>
