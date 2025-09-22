75b7adb50e9dab316a7a5e59ce844b2a11b77896
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-09-22 22:48:17 +0545

    Auto commit changes

--- public/js/admin/jobs/jobs.js
+++ public/js/admin/jobs/jobs.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
     // Summernote Init
-    $(".summernote").summernote({
-        height: 300
-    });
+    $(".summernote").summernote({ height: 300 });
 
     // DataTable Init
     var table = $("#show-job-data").DataTable({
@@ -10,32 +8,25 @@ $(document).ready(function () {
         serverSide: true,
         ajax: "/admin/jobs",
         lengthMenu: [[10, 25, 50, -1], [10, 25, 50, "All"]],
-        order: [[1, "asc"]], // fixed ordering
+        order: [[1, "asc"]],
         columns: [
-            { data: "DT_RowIndex", name: "DT_RowIndex", orderable: false, searchable: false },
-            { data: "image", name: "image", orderable: false, searchable: false },
-            { data: "title", name: "title" },
+            { data: "DT_RowIndex", orderable: false, searchable: false },
+            { data: "image", orderable: false, searchable: false },
+            { data: "title" },
             { data: "employer", name: "employer.name" },
-            { data: "location", name: "location" },
-            { data: "salary", name: "salary" },
-            { data: "status", name: "status", orderable: false, searchable: false },
-            { data: "action", name: "action", orderable: false, searchable: false }
+            { data: "location" },
+            { data: "salary" },
+            { data: "status", orderable: false, searchable: false },
+            { data: "action", orderable: false, searchable: false }
         ],
         buttons: [
-            {
-                extend: "print",
-                exportOptions: { columns: [0, 2, 3, 4, 5, 6] }
-            },
-            {
-                extend: "excel",
-                title: "",
-                exportOptions: { columns: [0, 2, 3, 4, 5, 6] }
-            }
+            { extend: "print", exportOptions: { columns: [0, 2, 3, 4, 5, 6] } },
+            { extend: "excel", title: "", exportOptions: { columns: [0, 2, 3, 4, 5, 6] } }
         ],
-        dom: '<"toolbar">Blfrtip' // ✅ only ONE dom key
+        dom: '<"toolbar">Blfrtip'
     });
 
-    // Toolbar Buttons
+    // Toolbar
     $("div.toolbar").html(`
         <button id="btnPrint" class="btn btn-primary mdi mdi-printer mdi-icon"></button>
         <button id="btnExport" class="btn btn-success mdi mdi-file-export mdi-icon"></button>
@@ -51,190 +42,151 @@ $(document).ready(function () {
         $("#validationErrors").addClass("d-none").html("");
         $("#jobDescription").summernote("code", "");
         $("#jobRequirements").summernote("code", "");
-        $(".form")[0].reset(); // reset form safely
+        $("#formModal form")[0].reset(); // ✅ safe reset
     }
 
-    // Add Job
+    // ========== ADD JOB ==========
     $(document).on("click", ".addJobBtn", function () {
         clearModal();
         $("#formModal").modal("show");
         $(".submitBtn").show();
         $(".updateBtn").hide();
-        $(".form").attr("id", "addForm");
+        $("#formModal form").attr("id", "addForm");
     });
 
-    $(document).off("submit", "#addForm").on("submit", "#addForm", function (event) {
-        event.preventDefault();
+    $(document).off("submit", "#addForm").on("submit", "#addForm", function (e) {
+        e.preventDefault();
         $(".submitBtn").prop("disabled", true);
 
         let formdata = new FormData(this);
 
         $.ajax({
-            type: "post",
+            type: "POST",
             url: "/admin/jobs/store",
             data: formdata,
             contentType: false,
             processData: false,
-            success: function (response) {
-                if (response.success) {
-                    Swal.fire({
-                        icon: "success",
-                        title: "Success",
-                        text: "Job Created Successfully",
-                        showConfirmButton: false,
-                        timer: 1000
-                    });
+            success: function (res) {
+                if (res.success) {
+                    Swal.fire({ icon: "success", title: "Success", text: "Job Created", showConfirmButton: false, timer: 1000 });
                     table.draw();
                     $("#formModal").modal("hide");
                 } else {
-                    Swal.fire({ icon: "warning", title: "Something went wrong!", text: "Please try again!" });
+                    Swal.fire({ icon: "warning", title: "Failed", text: "Please try again!" });
                 }
             },
             error: function (xhr) {
                 if (xhr.status === 422) {
                     let errors = xhr.responseJSON.errors;
-                    let errorMessages = "<ul>";
-                    $.each(errors, (key, value) => errorMessages += `<li>${value[0]}</li>`);
-                    errorMessages += "</ul>";
-                    $("#validationErrors").removeClass("d-none").html(errorMessages);
+                    let html = "<ul>";
+                    $.each(errors, (k, v) => (html += `<li>${v[0]}</li>`));
+                    html += "</ul>";
+                    $("#validationErrors").removeClass("d-none").html(html);
                 }
             },
             complete: () => $(".submitBtn").prop("disabled", false)
         });
     });
 
-    // Edit Job
+    // ========== EDIT JOB ==========
     $(document).on("click", ".editJobButton", function () {
         clearModal();
         $("#formModal").modal("show");
         $(".submitBtn").hide();
         $(".updateBtn").show();
-        $(".form").attr("id", "updateForm");
+        $("#formModal form").attr("id", "updateForm");
 
-        var id = $(this).attr("data-id");
+        var id = $(this).data("id");
 
-        $.ajax({
-            type: "get",
-            url: "/admin/jobs/detail/" + id,
-            success: function (response) {
-                $("#title").val(response.message.title);
-                $("#location").val(response.message.location);
-                $("#salary").val(response.message.salary);
-                $("#link").val(response.message.link);
-                $("#icon_class").val(response.message.icon_class);
-                $("#jobDescription").summernote("code", response.message.description);
-                $("#jobRequirements").summernote("code", response.message.requirements);
+        $.get("/admin/jobs/" + id, function (res) {
+            $("#title").val(res.message.title);
+            $("#location").val(res.message.location);
+            $("#salary").val(res.message.salary);
+            $("#link").val(res.message.link);
+            $("#icon_class").val(res.message.icon_class);
+            $("#jobDescription").summernote("code", res.message.description);
+            $("#jobRequirements").summernote("code", res.message.requirements);
 
-                if (response.message.image) {
-                    $("#jobImage").html(
-                        `<img src="/uploads/${response.message.image}" alt="Job Image" width="100" height="100" onerror="this.src='/defaultimage/defaultimage.webp';">`
-                    );
-                }
-                if (response.message.pdf) {
-                    $("#jobPdf").html(
-                        `<a href="/uploads/${response.message.pdf}" target="_blank" class="btn btn-sm btn-info">View PDF</a>`
-                    );
-                }
+            if (res.message.image) {
+                $("#jobImage").html(`<img src="/uploads/${res.message.image}" width="100" height="100" onerror="this.src='/defaultimage/defaultimage.webp';">`);
+            }
+            if (res.message.pdf) {
+                $("#jobPdf").html(`<a href="/uploads/${res.message.pdf}" target="_blank" class="btn btn-sm btn-info">View PDF</a>`);
             }
         });
 
-        $("#updateForm").off("submit").on("submit", function (event) {
-            event.preventDefault();
+        // submit update
+        $(document).off("submit", "#updateForm").on("submit", "#updateForm", function (e) {
+            e.preventDefault();
             $(".updateBtn").prop("disabled", true);
 
             let formdata = new FormData(this);
+            formdata.append("_method", "PUT"); // ✅ important for Laravel
 
             $.ajax({
-                type: "post",
-                url: "/admin/jobs/update/" + id,
+                type: "POST",
+                url: "/admin/jobs/" + id,
                 data: formdata,
                 processData: false,
                 contentType: false,
-                success: function (response) {
-                    if (response.success) {
-                        Swal.fire({
-                            icon: "success",
-                            title: "Success",
-                            text: "Job Updated Successfully",
-                            showConfirmButton: false,
-                            timer: 1000
-                        });
+                success: function (res) {
+                    if (res.success) {
+                        Swal.fire({ icon: "success", title: "Updated", showConfirmButton: false, timer: 1000 });
                         table.draw();
                         $("#formModal").modal("hide");
                     } else {
-                        Swal.fire({ icon: "warning", title: "Something went wrong!", text: "Please try again!" });
+                        Swal.fire({ icon: "warning", title: "Failed", text: "Please try again!" });
                     }
-                },
-                error: function () {
-                    Swal.fire({ icon: "warning", title: "Something went wrong!", showConfirmButton: false, timer: 1500 });
                 },
                 complete: () => $(".updateBtn").prop("disabled", false)
             });
         });
     });
 
-    // Status Toggle
+    // ========== TOGGLE STATUS ==========
     $(document).on("change", ".statusIdData", function () {
         let id = $(this).data("id");
-        let checkbox = $(this);
-        checkbox.prop("disabled", true);
+        let checkbox = $(this).prop("disabled", true);
 
         Swal.fire({
             icon: "warning",
             title: "Are you sure?",
             showCancelButton: true,
-            cancelButtonColor: "#d33",
-            confirmButtonColor: "#3085d6",
             confirmButtonText: "Yes, Change it!"
         }).then((result) => {
             if (result.isConfirmed) {
-                $.ajax({
-                    type: "get",
-                    url: "/admin/jobs/status/" + id,
-                    success: function () {
-                        checkbox.prop("disabled", false);
-                        table.draw();
-                    },
-                    error: () => checkbox.prop("disabled", false)
-                });
+                $.get("/admin/jobs/status/" + id, function () {
+                    checkbox.prop("disabled", false);
+                    table.draw();
+                }).fail(() => checkbox.prop("disabled", false));
             } else {
                 checkbox.prop("disabled", false).prop("checked", !checkbox.prop("checked"));
             }
         });
     });
 
-    // Delete Job
+    // ========== DELETE JOB ==========
     $(document).on("click", ".deleteData", function () {
-        let id = $(this).attr("data-id");
+        let id = $(this).data("id");
 
         Swal.fire({
             icon: "warning",
-            title: "Are you Sure?",
+            title: "Are you sure?",
             text: "You won't be able to revert this!",
             showCancelButton: true,
-            confirmButtonText: "Yes, Delete it!",
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33"
+            confirmButtonText: "Yes, Delete it!"
         }).then((result) => {
             if (result.isConfirmed) {
                 $.ajax({
-                    type: "get",
-                    url: "/admin/jobs/delete/" + id,
-                    success: function (response) {
-                        if (response.success) {
-                            Swal.fire({
-                                icon: "success",
-                                title: "Job Deleted Successfully",
-                                showConfirmButton: false,
-                                timer: 1500
-                            });
+                    type: "POST",
+                    url: "/admin/jobs/" + id,
+                    data: { _method: "DELETE", _token: $('meta[name="csrf-token"]').attr("content") },
+                    success: function (res) {
+                        if (res.success) {
+                            Swal.fire({ icon: "success", title: "Deleted", showConfirmButton: false, timer: 1500 });
                             table.draw();
-                        } else {
-                            Swal.fire({ icon: "warning", title: "Something went wrong!", text: "Please try again!" });
                         }
-                    },
-                    error: () =>
-                        Swal.fire({ icon: "warning", title: "Something went wrong!", showConfirmButton: false, timer: 1500 })
+                    }
                 });
             }
         });
