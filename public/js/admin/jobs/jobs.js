930cad27af5a7069a82bbd27f7bb8cde502da8ef
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-09-22 23:28:25 +0545

    Auto commit changes

--- public/js/admin/jobs/jobs.js
+++ public/js/admin/jobs/jobs.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+    $.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
     // Summernote Init
     $(".summernote").summernote({ height: 300 });
 
