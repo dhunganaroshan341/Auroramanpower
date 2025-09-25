7f3ec7eb9e2dfd0f4d6358a1c7dddd5ebbfb374a
dhunganaroshan341 <dhunganaroshan341@gmail.com>
2025-09-25 15:51:54 +0545

    Auto commit changes

--- resources/views/frontend/pages/jobseeker/upload-resume.blade.php
+++ resources/views/frontend/pages/jobseeker/upload-resume.blade.php
@@ -24,8 +24,8 @@
         <div class="auto-container">
             <!-- Section Title -->
             <div class="sec-title centred pb_70 sec-title-animation animation-style2">
-                <span class="sub-title mb_10 title-animation">SHARE YOUR PROFILE</span>
-                <h2 class="title-animation">Upload Your CV</h2>
+                <span class="sub-title mb_10 title-animation">SHARE YOUR DETAILS</span>
+                <h2 class="title-animation">Login & Upload Your CV</h2>
                 <p class="title-animation">Provide your details and CV so we can match you with suitable opportunities.</p>
             </div>
 
